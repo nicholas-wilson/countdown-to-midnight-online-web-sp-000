da8b6cff8ff2dec025928e0c507e95fd543d3983
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 19:09:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(timeleft)
+  while timeleft > 0
+    puts "#{timeleft} SECOND(S)!"
+    timeleft -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(time_in_seconds)
+  sleep(time_in_seconds)
 end
